dcc316c0be135aff65e0b5f7142ab9d45b6d49d1
Liz Colavita <lizcolavita@Lizs-MacBook-Air.local>
2022-11-03 10:46:25 -0500

    updated stacked and ring files

--- week_06/ring/ring.js
+++ week_06/ring/ring.js
@@ -6,8 +6,8 @@ d3.json('budget-2022.json').then((data) => {
       labelRadius = 200;
   
     const arcs = d3.pie().value(d => d.amount)(data);
-    const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius);
-    const arcLabel = d3.arc().innerRadius(labelRadius).outerRadius(labelRadius);
+    const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius); //generator that creates a generator
+    const arcLabel = d3.arc().innerRadius(labelRadius).outerRadius(labelRadius); //generator that creates a generator just another layer
   
     const svg = d3.select("#chart")
       .append("svg")
@@ -17,37 +17,39 @@ d3.json('budget-2022.json').then((data) => {
       .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
   
     svg.append("g")
-      .attr("stroke", "white")
-      .attr("stroke-width", 2)
-      .attr("stroke-linejoin", "round")
+      .attr("stroke", "white") // related to the space in between the segments (arcs)
+      .attr("stroke-width", 2) // related to the space in between the segments (arcs)
+      .attr("stroke-linejoin", "round") // related to the space in between the segments (arcs)
       .selectAll("path")
       .data(arcs)
       .join("path")
-      .attr("fill", (d, i) => d3.schemeCategory10[i])
+      .attr("fill", (d, i) => d3.schemeCategory10[i]) //automatic colors generated in d3
       .attr("d", arc);
   
-    // svg.append("g")
-    //   .attr("font-size", 10)
-    //   .attr("text-anchor", "middle")
-    //   .selectAll("text")
-    //   .data(arcs)
-    //   .join("text")
-    //   .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
-    //   .selectAll("tspan")
-    //   .data(d => {
-    //     return [d.data.category, d.data.amount];
-    //   })
-    //   .join("tspan")
-    //   .attr("x", 0)
-    //   .attr("y", (d, i) => `${i * 1.1}em`)
-    //   .attr("font-weight", (d, i) => i ? null : "bold")
-    //   .text(d => d);
+    svg.append("g")
+      .attr("font-size", 10)
+      .attr("text-anchor", "middle")
+      .selectAll("text")
+      .data(arcs)
+      .join("text")
+      .attr("transform", d => `translate(${arcLabel.centroid(d)})`) //positions the label at the center
+      .selectAll("tspan")
+      .data(d => {
+        return [d.data.category, d.data.amount];
+      })
+      .join("tspan")
+      .attr("x", 0)
+      .attr("y", (d, i) => `${i * 1.1}em`)
+      .attr("font-weight", (d, i) => i ? null : "bold")
+      .text(d => d);
   
-    // svg.append("text")
-    //   .attr("font-size", 30)
-    //   .attr("font-weight", "bold")
-    //   .attr("text-anchor", "middle")
-    //   .attr("alignment-baseline", "middle")
-    //   .text("2022")
-    //   .style("font-size", 20);
+    svg.append("text")
+      .attr("font-size", 30)
+      .attr("font-weight", "bold")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .text("2022")
+      .style("font-size", 30);
   });
+
+  /* see documentation for startAngle and endAngle - useful for building something like an odometer */
