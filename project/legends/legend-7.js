a13e15969385fb856b269a8d9dfa6183582e7ccb
Liz Colavita <89871328+LizCColavita@users.noreply.github.com>
2022-12-05 16:11:22 -0600

    Update legend-7.js

--- project/legends/legend-7.js
+++ project/legends/legend-7.js
@@ -1,10 +1,10 @@
 var svg = d3.select("#legend-7")
       .append("svg")
-      .attr("viewBox", [0, 0, 500, 10]);
+      .attr("viewBox", [0, 0, 400, 10]);
 
-    svg.append("circle").attr("cx",5).attr("cy",5).attr("r", 2).style("fill", "#ca0020")
-    svg.append("circle").attr("cx",75).attr("cy",5).attr("r", 2).style("fill", "#0571b0")
-    svg.append("circle").attr("cx",145).attr("cy",5).attr("r", 2).style("fill", "#c2a5cf")
-    svg.append("text").attr("x", 15).attr("y", 5).text("Republican").style("font-size", "6px").attr("alignment-baseline","middle")
-    svg.append("text").attr("x", 85).attr("y", 5).text("Democrat").style("font-size", "6px").attr("alignment-baseline","middle")
-    svg.append("text").attr("x", 155).attr("y", 5).text("Other").style("font-size", "6px").attr("alignment-baseline","middle")
+    svg.append("circle").attr("cx",5).attr("cy",5).attr("r", 3).style("fill", "#ca0020")
+    svg.append("circle").attr("cx",65).attr("cy",5).attr("r", 3).style("fill", "#0571b0")
+    svg.append("circle").attr("cx",125).attr("cy",5).attr("r", 3).style("fill", "#c2a5cf")
+    svg.append("text").attr("x", 12).attr("y", 5).text("Republican").style("font-size", "6px").attr("alignment-baseline","middle")
+    svg.append("text").attr("x", 72).attr("y", 5).text("Democrat").style("font-size", "6px").attr("alignment-baseline","middle")
+    svg.append("text").attr("x", 132).attr("y", 5).text("Other").style("font-size", "6px").attr("alignment-baseline","middle")
